25590c0d19b41dfb20c4d8617d875f41379593be
Muzzammil Afzal <muzzu2605afzall@gmail.com>
2026-06-12 01:56:54 +0530

    ui update3

--- components/GoToAdminButton.jsx
+++ components/GoToAdminButton.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const GoToAdminButton = () => {
   return (
      <button onClick={() => window.location.href = '/admin/login'}
-      className="w-full rounded-lg bg-blue-500 py-3 m-10 text-lg font-semibold text-white transition hover:bg-red-700">
+      className="w-full rounded-lg bg-blue-500 py-3  text-lg font-semibold text-white transition hover:bg-blue-600">
         Go to Admin Dashboard
           </button>
   )
